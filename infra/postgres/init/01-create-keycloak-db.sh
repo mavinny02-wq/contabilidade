66c6dff1589ba92823dbd783cd1e5511609eaaac
mavinny02-wq <mavinny02@googlemail.com>
2026-08-10 19:58:05 -0300

    fix: torna bootstrap do banco Keycloak idempotente

--- infra/postgres/init/01-create-keycloak-db.sh
+++ infra/postgres/init/01-create-keycloak-db.sh
@@ -5,13 +5,22 @@ DB_NAME="${KEYCLOAK_DB:-keycloak}"
 
 echo "Ensuring PostgreSQL database exists: $DB_NAME"
 
-psql -v ON_ERROR_STOP=1 \
-  --username "$POSTGRES_USER" \
-  --dbname "$POSTGRES_DB" \
-  --set=keycloak_db="$DB_NAME" <<'EOSQL'
-SELECT format('CREATE DATABASE %I', :'keycloak_db')
-WHERE NOT EXISTS (
-  SELECT 1 FROM pg_database WHERE datname = :'keycloak_db'
-)
-\gexec
+DATABASE_EXISTS="$(
+  psql -v ON_ERROR_STOP=1 \
+    --username "$POSTGRES_USER" \
+    --dbname "$POSTGRES_DB" \
+    --set=keycloak_db="$DB_NAME" \
+    --tuples-only \
+    --no-align <<'EOSQL'
+SELECT 1
+FROM pg_database
+WHERE datname = :'keycloak_db';
 EOSQL
+)"
+
+if [ "$DATABASE_EXISTS" != "1" ]; then
+  createdb \
+    --username "$POSTGRES_USER" \
+    --maintenance-db "$POSTGRES_DB" \
+    "$DB_NAME"
+fi
