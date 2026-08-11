01985fe9257a925116d123c7f52c7f62fbee2c64
mavinny02-wq <mavinny02@googlemail.com>
2026-08-10 23:37:52 -0300

    style: carrega estilos da Console Técnica

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './i18n';
 import './styles/global.css';
+import './styles/console-tecnica.css';
 import App from './App';
 import { AuthProvider } from './auth/AuthProvider';
 
